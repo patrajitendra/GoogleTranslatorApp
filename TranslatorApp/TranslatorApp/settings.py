003b445d1257d6c262715bc13aa21a67d657fe67
Jitendra Kumar Patra <40471061+patrajitendra@users.noreply.github.com>
2021-03-31 18:54:43 +0530

    Update settings.py

--- TranslatorApp/TranslatorApp/settings.py
+++ TranslatorApp/TranslatorApp/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'TranslatorApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Translator',
         'HOST' : 'localhost',
         'PORT' : '5432',
